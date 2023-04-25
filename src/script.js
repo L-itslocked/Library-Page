2b1bb79ff4a1b8d80cf3ef5687bf659d4706440f
L-ItsLocked <L-itslocked@users.noreply.github.com>
2023-04-25 02:37:51 -0400

    update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,11 @@
+// var bookImage = "book" + (i + 1);
+// var imageRow = document.createElement("div");
+// imageRow.classname = "row imageRow";
+
+// document.createElement("img");
+// img.src = data.item[i].volumeInfo.imageLinks.smallThumbnail;
+// document.getElementsByClassName("imageRow")[i].appendChild(img);
+
 function displaySearchResults(response) {
   let displayBook = document.querySelector("#display-book");
   let searchInput = document.querySelector("#searchinput").value;
@@ -13,6 +21,7 @@ function getData(event) {
 
 let submitEvent = document.querySelector("#submit");
 submitEvent.addEventListener("click", getData);
+
 
 //work on this script - figure out how to display book cover photo & info
 //example to check out: https://codepen.io/karenhenwood/pen/ybQYxO
